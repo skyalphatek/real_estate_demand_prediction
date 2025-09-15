1d363719a128128bd970506c4222e85785d9590c
jennifer-0401 <jenni40728@gmail.com>
2025-09-15 23:49:29 +0800

    Sep 15 uploaded
    -Create new features
    -Add lag features
    -models: Ridge
    -Optuna for hyperparameter

---
+++ china_real_estate.py
@@ -0,0 +1,13 @@
+# packages-EDA
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# packages- time series and models
+from sklearn.model_selection import TimeSeriesSplit
+import lightgbm as lgb
+import xgboost as xgb
+from statsmodels.tsa.arima.model import ARIMA
+from pathlib import Path
+
